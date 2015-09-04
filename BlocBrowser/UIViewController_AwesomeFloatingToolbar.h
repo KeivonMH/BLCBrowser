b50f8999e3dbb133f6c43cdd2bc30bd4a577aa73
Keivon Hobeheidar <KeivonMH@gmail.com>
2015-09-03 20:19:34 -0700

    AwesomeToolbar Error

---
+++ BlocBrowser/UIViewController_AwesomeFloatingToolbar.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController_AwesomeFloatingToolbar.h
+//  
+//
+//  Created by Keivon Hobeheidar on 9/3/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController ()
+
+@end
